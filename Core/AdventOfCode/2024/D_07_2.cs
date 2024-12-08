d06dc6bd9fc277601a3b5648d4eb36a70e8cf218
Owain Richardson <owain.richardson@gmail.com>
2024-12-08 14:24:48 +0000

    Complete day 7

--- Core/AdventOfCode/2024/D_07_2.cs
+++ Core/AdventOfCode/2024/D_07_2.cs
@@ -23,27 +23,29 @@ namespace AdventOfCode._2024
 
                 List<int> values = split[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(s => int.Parse(s)).ToList();
 
-                int numberOfOptions = (int)Math.Pow(2, values.Count - 1);
-                int lengthOfMaxString = Convert.ToString(numberOfOptions - 1, 2).Length;
+                int combinationLength = values.Count - 1;
+                string combination = "".PadLeft(combinationLength, '0');
 
-                for (int i = 0; i < numberOfOptions; i++)
+                while (!string.IsNullOrEmpty(combination))
                 {
-                    string binary =  Convert.ToString(i, 2).PadLeft(lengthOfMaxString, '0');
-
-                    int binaryIndex = 0;
+                    int combinationIndex = 0;
                     long total = values[0];
                     for (int index = 1; index <= values.Count - 1; index++)
                     {
-                        if (binary[binaryIndex] == '0')
+                        if (combination[combinationIndex] == '0')
                         {
                             total += values[index];
                         }
-                        else if (binary[binaryIndex] == '1')
+                        else if (combination[combinationIndex] == '1')
                         {
                             total *= values[index];
                         }
+                        else if (combination[combinationIndex] == '2')
+                        {
+                            total = long.Parse($"{total}{values[index]}");
+                        }
 
-                        binaryIndex++;
+                        combinationIndex++;
                     }
 
                     if (total == target)
@@ -52,98 +54,29 @@ namespace AdventOfCode._2024
                         break;
                     }
 
+                    combination = CalculateCombination(combination, 0, 2, combination.Length - 1);
                 }
             }
 
             Console.WriteLine(answer);
         }
 
-        private static (int x, int y) Move(Direction direction, int x, int y) => direction switch
+        private static string CalculateCombination(string combination, int min, int max, int startIndex)
         {
-            Direction.Up => (x, y - 1),
-            Direction.Right => (x + 1, y),
-            Direction.Down => (x, y + 1),
-            Direction.Left => (x - 1, y),
-            _ => throw new InvalidOperationException()
-        };
+            if (startIndex < 0) return null;
 
-        private static Direction TurnRight(Direction direction) => direction switch
-        {
-            Direction.Up => Direction.Right,
-            Direction.Right => Direction.Down,
-            Direction.Down => Direction.Left,
-            Direction.Left => Direction.Up,
-            _ => throw new InvalidOperationException()
-        };
+            int[] split = combination.ToCharArray().Select(c => int.Parse(c.ToString())).ToArray();
 
-        private static bool ObjectInFront(char[,] map, int x, int y, Direction direction, int length)
-        {
-            if (direction == Direction.Up && y - 1 >= 0 && map[x, y - 1] == '#')
+            split[startIndex] += 1;
+
+            if (split[startIndex] > max)
             {
-                return true;
+                split[startIndex] = 0;
+
+                return CalculateCombination(string.Join(null, split), min, max, startIndex - 1);
             }
 
-            if (direction == Direction.Right && x + 1 < length && map[x + 1, y] == '#')
-            {
-                return true;
-            }
-
-            if (direction == Direction.Down && y + 1 < length && map[x, y + 1] == '#')
-            {
-                return true;
-            }
-
-            if (direction == Direction.Left && x - 1 >= 0 && map[x - 1, y] == '#')
-            {
-                return true;
-            }
-
-            return false;
-        }
-
-        private static void DrawMap(char[,] map, int length)
-        {
-            Console.WriteLine();
-
-            for (int y = 0; y < length; y++)
-            {
-                for (int x = 0; x < length; x++)
-                {
-                    Console.Write(map[x, y]);
-                }
-                Console.WriteLine();
-            }
-        }
-
-        private static (char[,] map, int x, int y) ParseInputs(string[] inputs)
-        {
-            char[,] map = new char[inputs[0].Length, inputs.Length];
-
-            int startX = -1;
-            int startY = -1;
-
-            int y = 0;
-            foreach (string input in inputs)
-            {
-                int x = 0;
-
-                foreach (char c in input)
-                {
-                    map[x, y] = c;
-
-                    if (c == '^')
-                    {
-                        startX = x;
-                        startY = y;
-                    }
-
-                    x++;
-                }
-
-                y++;
-            }
-
-            return (map, startX, startY);
+            return string.Join(null, split);
         }
     }
 }
